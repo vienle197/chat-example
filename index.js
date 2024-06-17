7e75787ffc023958c59da3d6a8c24843f4cd3102
Viễn Lê <33427571+vienle197@users.noreply.github.com>
2024-06-17 19:20:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,9 @@ if (cluster.isPrimary) {
     }
   });
 
-  const port = process.env.PORT;
+ /// const port = process.env.PORT;
+
+  const port = 3000;
 
   server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
